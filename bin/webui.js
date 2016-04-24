b8220ce961df8447a64b86eef4ca5ef74a714672
Rafał Mikołajun <rafal@mikoweb.pl>
2016-04-24 18:25:57 +0200

    dodano grunt

--- bin/webui.js
+++ bin/webui.js
@@ -5,6 +5,7 @@
 var program = require('commander'),
     config = require('../lib/config.js'),
     bower = require('../lib/bower.js'),
+    grunt = require('../lib/grunt.js'),
     action, settings;
 
 program
@@ -22,9 +23,11 @@ settings = config.getConfig(process.cwd() + '/webui.json');
 switch (action) {
     case 'install':
         bower.execute(settings, bower.actions.install, program.onlyVendor || false);
+        grunt.execute(settings);
         break;
     case 'update':
         bower.execute(settings, bower.actions.update, program.onlyVendor || false);
+        grunt.execute(settings);
         break;
     default:
         console.error('no command given!');
---
+++ template/Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        path: {
+            webui: {
+                vendor: '{{path_webui_vendor}}'
+            }
+        },
+        pkg: grunt.file.readJSON('{{path_package_js}}'),
+        clean: {
+            webuiVendor: ['<%= path.webui.vendor %>']
+        },
+        copy: {
+            webuiVendor: {{copy_webui_vendor}}
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', [
+        'clean:webuiVendor',
+        'copy:webuiVendor'
+    ]);
+};
